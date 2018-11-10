09d31239a1d0cf6cb5b75bcbc375b7ded037be9a
shino <harimanmon@gmail.com>
2018-11-10 10:24:52 +0900

    refactor: use let

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -16,17 +16,15 @@ RSpec.describe Note, type: :model do
 
   # 文字列に一致するメッセージを検索する
   describe 'search message for a term' do
-    before do
-      @note1 = FactoryBot.create(:note, message: 'This is the first note')
-      @note2 = FactoryBot.create(:note, message: 'This is the second note')
-      @note3 = FactoryBot.create(:note, message: 'First, preheat the oven.')
-    end
+    let(:note1) { FactoryBot.create(:note, message: 'This is the first note') }
+    let(:note2) { FactoryBot.create(:note, message: 'This is the second note') }
+    let(:note3) { FactoryBot.create(:note, message: 'First, preheat the oven.') }
 
     # 一致するデータがみつかるとき
     context 'when a match is found' do
       # 検索文字列に一致するメモを返すこと
       it 'returns notes that match the search term' do
-        expect(Note.search('first')).to include(@note1, @note3)
+        expect(Note.search('first')).to include(note1, note3)
       end
     end
 
